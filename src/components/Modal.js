76e3415df2338c6118602e4209b1cafdc9ddd63f
ImGyuriKim <gyurikim0105@gmail.com>
2023-06-23 16:25:05 +0900

    feat: add Modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,36 @@
-import React from "react";
+import styled from "styled-components";
 
-function Modal() {
-  // 영화 디테일 정보 네트워크 요청
-  // 클릭 핸들러 -> isModalOpen 상태관리
+const ModalContainer = styled.div`
+ position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.6);
+  width: 80%;
+  max-width: 600px;
+  padding: 20px;
+  border-radius: 5px;
+  color: white;
+`
+const MovieTitle = styled.h2`
+  text-align: center;
+`;
 
-  // 영화 정보 렌더링
-  return <div>영화 디테일</div>;
+ // 영화 정보 렌더링
+function Modal(props) {
+
+const { movieData, setIsModalOpen } = props;
+
+const closeModal = () => {
+  setIsModalOpen(false);
+};
+  return (
+  <ModalContainer onClick={closeModal}>
+    <MovieTitle>Title: {movieData.original_title}</MovieTitle>
+    {/* *{//장르, overview, 포스터, 개봉일, 평점 추가}* */}
+  </ModalContainer>
+  )
+  
 }
 
 export default Modal;
